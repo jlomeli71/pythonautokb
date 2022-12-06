4666e70d5fc83020d1ff1ae16edaeaf8f41a3b7e
Jose M. Lomeli <jmlomeli@hotmail.com>
2022-12-05 17:58:37 -0800

    added more execrcises

---
+++ week3/exer33.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+import json
+from pprint import pprint
+
+with open("my_json.json", "r") as file:
+    json_data = json.loads(file.read())
+
+#print(f"Data file is: {type(json_data)}")
+#pprint(json_data)
+
+ipv4_list = []
+ipv6_list = []
+
+for intf, ipaddr_dict in json_data.items():
+    for ipv4_or_ipv6, addr_info in ipaddr_dict.items():
+        for ip_addr, prefix_dict in addr_info.items():
+            prefix_length = prefix_dict["prefix_length"]
+            if ipv4_or_ipv6 == "ipv4":
+                ipv4_list.append("{}/{}".format(ip_addr, prefix_length))
+            elif ipv4_or_ipv6 == "ipv6":
+                ipv6_list.append("{}/{}".format(ip_addr, prefix_length))
+
+print("\nIPv4 Addresses: {}".format(ipv4_list))
+print("\nIPv6 Addresses: {}".format(ipv6_list))
+
